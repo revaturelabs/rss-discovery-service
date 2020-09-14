8939e55c0e6b0f728559a8fbea8d8e6529905654
Eurus Wagner <euruswagner@gmail.com>
2020-09-14 13:05:04 -0400

    changed test/port back

--- rss-discovery-service/src/test/java/com/revature/discovery/DiscoveryApplicationTests.java
+++ rss-discovery-service/src/test/java/com/revature/discovery/DiscoveryApplicationTests.java
@@ -1,26 +1,13 @@
 package com.revature.discovery;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//@SpringBootTest
-//class DiscoveryApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+@SpringBootTest
+class DiscoveryApplicationTests {
 
-@SpringBootApplication
-@EnableEurekaServer
-public class DiscoveryApplicationTests {
-
-    public static void main(String[] args) {
-        SpringApplication.run(DiscoveryApplication.class, args);
+    @Test
+    void contextLoads() {
     }
 
 }
